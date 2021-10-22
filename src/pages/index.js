6fef65628d2976a4354e77555e2ffd3c62e162b9
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-22 13:29:40 -0700

    hello world starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Stephen Tamiesie</h2>
+        <h3>Software Engineer</h3>
+        <p>You've tried the best, now try the rest.</p>
+      </div>
+    </section>
+  )
 }
