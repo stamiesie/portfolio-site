0323316d2c4b2cc4a8d529272aaa8b3ea296154f
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-25 14:22:27 -0700

    add dynamic image in project component for feature image using gatsby-plugin-image

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
 import * as styles from '../../styles/projects.module.css'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
     console.log(data)
@@ -17,6 +18,8 @@ export default function Projects({ data }) {
                     {projects.map(project => (
                         <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <GatsbyImage image={getImage(project.frontmatter.thumbnail.childImageSharp.gatsbyImageData)} 
+                                alt="Banner" />
                                 <h3>{ project.frontmatter.title }</h3>
                                 <p>{ project.frontmatter.stack }</p>
                             </div>
@@ -30,21 +33,30 @@ export default function Projects({ data }) {
 
 // export page query
 export const query = graphql`
-    query ProjectsPage {
-        projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-            nodes {
-              frontmatter {
-                stack
-                title
-                slug
-              }
-              id
+query ProjectsPage {
+    projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          stack
+          title
+          slug
+          thumbnail {
+            childImageSharp {
+              gatsbyImageData(
+                  layout: FULL_WIDTH, 
+                  placeholder: BLURRED, 
+                  formats: [AUTO, WEBP]
+                  )
             }
           }
-          contact: site {
-              siteMetadata {
-                  contact
-              }
-          }
         }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
   `
--- src/projects/whatll-ya-have.md
+++ src/projects/whatll-ya-have.md
@@ -3,6 +3,8 @@ title: What'll Ya Have
 stack: React, Amazon S3 - list other things here...
 slug: whatll-ya-have
 date: 2021-03-30T00:00:00+00:00
+thumbnail: ../images/thumbs/what'll_ya_have_500.png
+# featuredImg: ../images/featured/image_name.png
 ---
 
 **Lorem ipsum dolor sit amet**, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
