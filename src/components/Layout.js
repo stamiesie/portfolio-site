f10ab8c8a8fd02d0609ccb3dee0fe2fd4154d02b
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-26 13:40:05 -0700

    add connection links to footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Navbar from './Navbar'
+import linkedIn from '../../public/linkedin.png'
+import gitHub from '../../public/github.png'
+import mail from '../../public/mail.png'
 import '../styles/global.css'
 
 export default function Layout({ children }) {
@@ -9,7 +12,39 @@ export default function Layout({ children }) {
             <div className="content">
                 { children }
             </div>
-            <footer>© 2021 Stephen Tamiesie</footer>
+            <footer>
+            <a
+              href="https://www.linkedin.com/in/stephen-tamiesie/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                className="linkedInLogo"
+                src={linkedIn}
+                alt="linkedIn"
+              />
+            </a>
+            <a
+              href="https://github.com/stamiesie"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                className="githubLogo"
+                src={gitHub}
+                alt="github"
+              />
+            </a>
+            <a
+              href="mailto:stephen.tamiesie@gmail.com"
+            >
+              <img
+                className="mail"
+                src={mail}
+                alt="mail"
+              />
+            </a>
+            </footer>
         </div>
     )
 }
