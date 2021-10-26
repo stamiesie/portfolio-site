fa1de1651306dd49b17e38733f7f24d454deeda3
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-26 16:40:38 -0700

    change home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <div>
           <h2>Stephen Tamiesie</h2>
           <h3>Software Engineer</h3>
-          <p>You've tried the best, now try the rest.</p>
+          <p>Dedicated to finding meaningful solutions using code.</p>
           <Link className={styles.btn} to="/projects">Check out some projects</Link>
         </div>
       </section>
