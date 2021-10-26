e27cdb09f2d3ee8c460e5d9281f48a3c8bed35b0
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-26 16:26:20 -0700

    add content, styling to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import * as styles from '../styles/about.module.css'
 
 export default function About() {
     return (
         <Layout>
-            <div>
-                <h1>About</h1>
-                <p>Hi, my name is Stephen.  I'm a software developer in Portland, Oregon.  Here are some things to know about me:</p>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className={styles.aboutContent}>
+                <h2 className={styles.header}>About</h2>
+                <p>Hello!  I'm an early career software engineer that is passionate about modern web development, outdoor adventures and my cat named Tuna.  Once I let Tuna outside and he came home 11 months later (no joke). I'm convinced he snuck into a time machine...</p>
                 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Before becoming a software engineer I was professional photographer, where I enjoyed taking concepts and turning them into creative assignments for clients.  This eventually let me to software development, where I still get to build and create fun stuff from scratch everyday.</p>
+
+                <p>I'm an AWS Certified Developer Associate and while I love the cloud, my heart lies in the front end.  I'm currently exploring the Jamstack (I built this site with Gatsby) and continuing to hone my React skills.     
+                </p>
+
+                <p>Have an extra ticket to the Blazers game?  Want to talk about medium format film cameras?  Think I'd be a good fit for your team?  Let's connect!</p>
             </div>
         </Layout>
     )
