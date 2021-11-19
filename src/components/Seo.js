a3250e5f2ef0488de2dc0fe4da4214e996148ad3
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-11-18 17:02:20 -0800

    revert to Node LTS 16.13.0 for compatibility with Gatsby

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,3 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const SEO = ({ title, description, image, article }) => {
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
+
+  return null;
+};
+
+export default SEO;
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+};
+
+SEO.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  article: false,
+};
+
+const query = graphql`
+`
